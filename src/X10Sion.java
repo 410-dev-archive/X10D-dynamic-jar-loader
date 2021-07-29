c597cdb9c9576f3fb0e9117453f00eaccb5a23a2
410 <58301450+410-dev@users.noreply.github.com>
2021-07-30 01:00:57 +0900

    Interface update

--- src/X10Sion.java
+++ src/X10Sion.java
@@ -1,8 +1,11 @@
 public interface X10Sion {
     public final String X10SION_VERSION = "A.1";
     public final int X10SION_COMPAT = 1;
-    public void getVersion();
-    public void getName();
-    public void getVendor();
+    public String X10SionVersion();
+    public int X10SionCompatibility();
+    public String getVersion();
+    public int getBuild();
+    public String getName();
+    public String getVendor();
     public int run(Object[] dynamicParameters);
 }
